b26bb83b3435dec8d9bba4078f998057177f7c26
noambrihun <noambrihun1@gmail.com>
2026-03-20 14:59:45 +0200

    I changed the file server.js : i removed the schema and the model and i puted them in other file

--- server.js
+++ server.js
@@ -1,47 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 const PORT = 3000;
 
 
-const app = express();
+const Snippet = require("./models/Snippet")
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
+app.use(cors())
+app.use(express.json())
 
-mongoose.connect("mongodb+srv://Iamnoam_23:kingbr123@noam.cyi4okn.mongodb.net/snippets")
-.then(() => {
-    console.log("MongoDB connected")
-})
-.catch((err) => {
-    console.log(err)
-})
+mongoose.connect("mongodb+srv://Iamnoam_23:kingbr123@noam.cyi4okn.mongodb.net/snippets").then(()=> console.log("MongoDB connected"))
+.catch(err => console.log(err))
 
-const snippetsSchema = new mongoose.Schema({
-title:String,
-language:String,
-code:[String],
-description:String,
-createdAt:Date,
+app.get("/", (req,res)=>{
+  res.send("Code Snippet Manager API is running")
 })
 
-const Snippet = mongoose.model("Recipe", snippetsSchema)
+app.post("/snippets", async (req,res)=>{
+  try{
 
-app.get('/', (req, res) => {
-  res.send('Code Snippet Manager API is running');
-});
+    const snippet = new Snippet(req.body)
 
+    await snippet.save()
 
-app.post('/snippets', async (req, res) => {
-  try {
-    const snippet = new Snippet(req.body);
-    await snippet.save();
-    res.status(201).json({ success: true, data: snippet });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error.message });
+    res.status(201).json({
+      success:true,
+      data:snippet
+    })
+
+  }catch(error){
+
+    res.status(400).json({
+      success:false,
+      error:error.message
+    })
+
   }
-});
+})
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
